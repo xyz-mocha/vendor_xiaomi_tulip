f611cdd27a0708f336cf6d515690aa799b2e4a01
daniml3 <danimoral1001@gmail.com>
2021-12-01 11:54:35 +0800

    tulip: Drop QTI datastatusnotification
    Change-Id: Ie24b17d34624df6c7ae2acdf69b610a0b10d21dd

--- tulip-vendor.mk
+++ tulip-vendor.mk
@@ -1105,7 +1105,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
